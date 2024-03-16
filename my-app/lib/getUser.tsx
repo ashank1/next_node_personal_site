775a854cc6f2e8bc5747ec6ab114cb2e0ee82b9e
Mateusz Topa <mati21612@outlook.com>
2024-03-16 00:59:43 +0000

    Revert "Initial commit"
    This reverts commit 0761585201a2c06767c29a9da33f902785d7a7da.

--- my-app/lib/getUser.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default async function getUser(userId: string) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
-    if(!res.ok) undefined 
-    return res.json()
-}
--- my-app/lib/getUserPosts.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default async function getUserPosts(userId: string) {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    if(!res.ok) undefined 
-    return res.json()
-}
